230ec65e52fe1f52e3c9ae2e0aaac90513b8baca
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-26 16:15:04 +0530

    Updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  //authmodule
+  {path:"login",component:LoginComponent},
+  {path:'Signup' ,component:SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
