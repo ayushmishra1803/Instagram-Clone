ce2ab19c598f71d62906b19ffaed1a7a573fe5d5
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-26 16:29:54 +0530

    Updates

--- src/app/module/material/material/material.module.ts
+++ src/app/module/material/material/material.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
-
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 @NgModule({
   declarations: [],
-  imports: [CommonModule, FlexLayoutModule],
-  exports: [FlexLayoutModule],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSnackBarModule,
+  ],
+  exports: [
+    FlexLayoutModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSnackBarModule,
+  ],
 })
 export class MaterialModule {}
