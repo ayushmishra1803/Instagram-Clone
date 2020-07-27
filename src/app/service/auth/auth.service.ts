941c412632cd1ee2fb039d6dec38b15e8a19088d
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-27 22:22:04 +0530

    Updates

--- src/app/service/auth/auth.service.ts
+++ src/app/service/auth/auth.service.ts
@@ -2,16 +2,18 @@ import { Signup } from './../../interface/auth/signup/signup';
 import { Login } from './../../interface/auth/login/login';
 import { Forgetpassword } from './../../interface/auth/forgetpassword/forgetpassword';
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  constructor(private db:AngularFirestore) {}
 
   login(data: Login) {
     console.log('login');
   }
   signup(data: Signup) {
+    this.db.collection("Users").add(data);
     console.log('signup');
   }
   forgetPassword(data: Forgetpassword) {
